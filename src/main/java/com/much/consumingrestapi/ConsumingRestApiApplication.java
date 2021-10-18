ccf9776835c23a9d21f416a0d6c9b2e883b6ad40
Much <ucheanna909@gmail.com>
2021-10-18 21:44:41 +0100

    initial commit

---
+++ src/main/java/com/much/consumingrestapi/ConsumingRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.much.consumingrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumingRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumingRestApiApplication.class, args);
+	}
+
+}
